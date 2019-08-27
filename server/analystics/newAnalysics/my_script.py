1a8e29cae9bc77aceb00102b26e8b141e8ca8b51
Gabi B <Gab3009@Gmail.com>
2019-08-27 16:38:55 +0300

    g

--- server/analystics/newAnalysics/my_script.py
+++ server/analystics/newAnalysics/my_script.py
@@ -1,7 +1,5 @@
 import sys
 import json
 import pyresparser
-import en_core_web_sm
-nlp = en_core_web_sm.load()
 data = json.dumps(pyresparser.ResumeParser(input()).get_extracted_data())
 print(data)
