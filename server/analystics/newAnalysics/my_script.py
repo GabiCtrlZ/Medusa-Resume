7d3a339c6dc8180a356dd22d1bcc10f6979558b9
Gabi B <Gab3009@Gmail.com>
2019-08-27 16:30:47 +0300

    spacy try

--- server/analystics/newAnalysics/my_script.py
+++ server/analystics/newAnalysics/my_script.py
@@ -1,5 +1,7 @@
 import sys
 import json
 import pyresparser
+import en_core_web_sm
+nlp = en_core_web_sm.load()
 data = json.dumps(pyresparser.ResumeParser(input()).get_extracted_data())
 print(data)
